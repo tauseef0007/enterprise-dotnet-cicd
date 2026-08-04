7c50b673c1891555110ba234c021c32b30448798
tauseef0007 <tauseef4962@gmail.com>
2026-08-04 16:40:18 +0530

    Added Network Module

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,21 @@ module "resource_group" {
   tags = local.common_tags
 
 }
+
+module "network" {
+
+  source = "./modules/network"
+
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
+
+  vnet_name = "vnet-${var.project_name}-${var.environment}"
+  vnet_cidr = "10.10.0.0/16"
+
+  app_subnet_name = "snet-app"
+  app_subnet_cidr = "10.10.1.0/24"
+
+  public_ip_name = "pip-${var.project_name}-${var.environment}"
+
+  tags = local.common_tags
+}
---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,32 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_cidr" {
+  type = string
+}
+
+variable "app_subnet_name" {
+  type = string
+}
+
+variable "app_subnet_cidr" {
+  type = string
+}
+
+variable "public_ip_name" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
